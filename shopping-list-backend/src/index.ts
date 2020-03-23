9f7b6b4a6782057f0b39b7ae24f25656a88ed3fe
Ted <ted.agner@hotmail.com>
2020-03-23 12:22:52 +0100

    removed cors. Using proxy instead.

--- shopping-list-backend/src/index.ts
+++ shopping-list-backend/src/index.ts
@@ -1,6 +1,5 @@
 import { config } from "dotenv";
 import * as express from "express";
-import * as cors from "cors";
 import * as helmet from "helmet";
 import router from "./api/endpoints";
 import { connectToDb } from "./config/configDb";
@@ -9,7 +8,6 @@ config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(helmet());
 app.use("/", router);
 
